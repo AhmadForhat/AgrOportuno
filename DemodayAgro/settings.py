abcfd00c5c4d6b43fe247692742a1e5267698ea5
AhmadForhat <ahmad.forhat@gmail.com>
2019-12-11 21:29:35 -0300

    Adicionando teste para arrumar o erro

--- DemodayAgro/settings.py
+++ DemodayAgro/settings.py
@@ -123,6 +123,13 @@ STATIC_URL = '/static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,MEDIA_URL)
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 import django_heroku
 
 django_heroku.settings(locals())    
